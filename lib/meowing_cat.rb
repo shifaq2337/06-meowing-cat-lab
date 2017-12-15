cac17e148fddd4a6d3fb0f610073bb005e007ee4
shifaq2337 <shifaq2337@hstat.org>
2017-12-15 00:32:58 +0000

    done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,6 @@
-## code your solution here. 
+class Cat 
+    attr_accessor :name #This set and gets name, you don't need to do this to meow b/c meow is not going to change, duh!
+    def meow #teaching the cat how to meow
+       puts "meow!" 
+    end
+end
